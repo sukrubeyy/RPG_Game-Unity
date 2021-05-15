60d085ff9d9cd23a19538ddb7faaab6fa2b292fe
sukrubeyy <sukru.beyy@outlook.com>
2021-05-15 17:56:15 +0300

    Player,Camera,Navmesh

---
+++ RPG-Projects-File/Assets/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    private float zoomCurrent = 10f;
+    private float  pitch=2f;
+
+    public float zoomSpeed = 4f;
+    public float maxZoom = 15f;
+    public float minZoom = 5f;
+
+    public float camRotateSpeed=150f;
+    private float inputCamHorizontal=0f;
+
+    private void Update()
+    {
+        zoomCurrent -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        zoomCurrent = Mathf.Clamp(zoomCurrent, minZoom, maxZoom);
+        inputCamHorizontal -= Input.GetAxis("Horizontal") * camRotateSpeed * Time.deltaTime;
+    }
+
+    private void LateUpdate()
+    {
+        //Aradaki farkı bulup zoomCurrent ile çarpıp kameramızın pozisyonunu belirliyoruz
+        transform.position = target.position - offset * zoomCurrent;
+        //Hedef objeye bakması için her frame'de döndürür
+        transform.LookAt(target.position+Vector3.up*pitch);
+        //
+        transform.RotateAround(target.position, Vector3.up, inputCamHorizontal);
+    }
+}
---
+++ RPG-Projects-File/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask moveMask;
+    Camera cam;
+    Ray ray;
+    RaycastHit hit;
+    PlayerMotor motor;
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+        cam = Camera.main;
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit,100,moveMask))
+            {
+                motor.MovePoint(hit.point);
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100, moveMask))
+            {
+               
+            }
+        }
+
+    }
+}
